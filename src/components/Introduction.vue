7164d2ae17b09d9bcc51f7ff4a459f14626a62ea
Paul Grant <paul@cssinate.com>
2019-10-10 07:46:32 -0400

    Adding intro text

---
+++ src/components/Introduction.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>What is this thing?</h2>
+    <p>When you type <code>ls</code> into your bash terminal, it lists files and directories. On most installations, those files and directories are styled to indicate some of their properties. The terminal can use 16 available colors. And the defaults are kind of ugly.</p>
+    <p>Most modern terminal applications can have definitions for those 16 colors applied to them. You can say "Show anything that the terminal thinks is Bright Red as <code>#c0ffee</code>.</p>
+    <p>This application helps you style different types of files and folders, AND it helps you generate a theme for your terminal application.</p>
+    <h2>How do I get started?</h2>
+    <p>Click one of the items on the right to edit its styling, choose to generate a theme for your terminal application.</p>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
